df178991c447a51b17ad5ca7452f53d0ec5a3f45
russo <raa.augusto@hotmail.com>
2023-11-21 19:54:13 -0300

    first commit

---
+++ src/loja_veiculos_jframe/Loja_Veiculos_JFrame.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package loja_veiculos_jframe;
+
+/**
+ *
+ * @author russo
+ */
+public class Loja_Veiculos_JFrame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
